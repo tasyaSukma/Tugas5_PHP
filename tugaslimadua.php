2e0deab3e333241a1175a513737dd7d3e89b6f23
tasyaSukma <maftuhah.tasya@gmail.com>
2020-10-08 10:33:13 +0700

    Update tugaslimadua.php

--- tugaslimadua.php
+++ tugaslimadua.php
@@ -1,6 +1,7 @@
 <?php
 	$input1 = ["Ayo", "Bersama"];
 	$input2 = ["Belajar", "Niomic"];
+	$gabungan="";
 
 	echo "<h2>Operator String</h2>";
 	echo "<table border=1>
@@ -11,6 +12,7 @@
 			</tr>";
 
 	for($i = 0 ; $i<count($input1) ; $i++){
+		$gabungan .= $input1[$i]." ".$input2[$i]." ";
 		echo "<tr><td>".$input1[$i]."</td>
 			  <td>$input2[$i]</td><td>";
 		echo $input1[$i]." ".$input2[$i];
@@ -18,4 +20,5 @@
 	}
 
 	echo "</table>";
+	echo "<br><h3>$gabungan<h3>";
 ?>
